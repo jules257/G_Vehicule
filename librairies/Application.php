c5e8f185c415345f0766267607c6930b70b31039
Jules257 <julesnzejimana@gmail.com>
2023-02-13 14:05:24 +0100

    add application

---
+++ librairies/Application.php
@@ -0,0 +1,20 @@
+<?php
+class Application{
+
+    public static function proccess(){
+        $controllerName="User";
+        $task="index";
+
+        if(!empty($_GET['controller'])){
+            $controllerName=ucfirst($_GET['controller']);
+        }
+
+        if(!empty($_GET['task'])){
+            $task=$_GET['task']; 
+        }
+        $controllerName="\Controllers\\".$controllerName;
+        $controller=new $controllerName();
+
+        $controller->$task();
+    }
+}
